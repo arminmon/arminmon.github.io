c55b9e719b20c0a352fa9aedb70cc9ef2e24073f
arminmon <arminmon@users.noreply.github.com>
2021-07-16 06:59:25 +0000

    deploy: 05d62a4303edab00ff60528f88fcf9fbd4dcbd48

--- _nuxt/static/1626415550/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626415550",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"faa62b19"}}}}(null,"\u002F"));
